4afc9ce714179aea21a5b3404563a329b8d8a7f8
to-omer <toomerhs@gmail.com>
2020-11-02 14:42:59 +0900

    refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -1,107 +1,142 @@
 import * as vscode from 'vscode';
 import * as cp from 'child_process';
 import * as fs from 'fs';
-import * as path from 'path';
 
-export function activate(context: vscode.ExtensionContext) {
-	const cache = getCacheFile();
-	if (cache === undefined) {
+export async function activate(context: vscode.ExtensionContext) {
+	try { await execShell('cargo --version'); } catch {
+		vscode.window.showErrorMessage("Codesnip: `cargo` not found");
+		return;
+	};
+	try { await execShell('cargo codesnip --version'); } catch {
+		vscode.window.showErrorMessage("Codesnip: `cargo-codesnip` not found.");
+		return;
+	};
+
+	const config = getCodesnipConfiguration();
+	if (config === undefined) {
 		vscode.window.showErrorMessage("Codesnip: No cache file is set");
 		return;
 	}
-	try { fs.statSync(path.dirname(cache)); } catch (err) {
-		fs.mkdir(path.dirname(cache), { recursive: true }, err => { vscode.window.showErrorMessage("Codesnip: " + err); });
-	}
-	try { fs.statSync(cache); } catch (err) {
-		try {
-			const fd = fs.openSync(cache, 'w');
-			fs.writeSync(fd, "{}");
-			fs.closeSync(fd);
-		} catch (err) {
-			vscode.window.showErrorMessage("Codesnip: " + err);
-		}
-		vscode.window.showInformationMessage("Codesnip: cache not found", "Update Cache").then(() => {
-			vscode.commands.executeCommand("codesnip-vscode.updateCache");
-		});
-	}
 
-	const codesnipcmd = `cargo codesnip --use-cache=${cache}`;
-	execShell(codesnipcmd + ' list').then(value => value.trimEnd().split(/\r\n|\n/)).then(items => {
-		const bundleDisposable = vscode.commands.registerCommand('codesnip-vscode.bundle', () => {
-			let options: vscode.QuickPickOptions = { placeHolder: "name" };
-			vscode.window.showQuickPick(items, options).then(bundle => {
-				if (bundle) {
+	const updateCacheDisposable = vscode.commands.registerCommand('codesnip-vscode.updateCache', () => {
+		vscode.window.withProgress({
+			location: vscode.ProgressLocation.Notification,
+			title: "Codesnip update cache"
+		}, progress => execShell(getUpdateCacheCommand(config)).then(() => {
+			progress.report({ increment: 1 });
+			const reloadItem = "Reload Now";
+			vscode.window.showInformationMessage("Codesnip: reload required", reloadItem).then(selection => {
+				if (selection === reloadItem) {
+					vscode.commands.executeCommand("workbench.action.reloadWindow");
+				}
+			});
+		}).catch(err => { vscode.window.showErrorMessage("Codesnip: " + err); }));
+	});
+	context.subscriptions.push(updateCacheDisposable);
+
+	try {
+		fs.statSync(config.cacheFile);
+		const codesnipcmd = `cargo codesnip --use-cache=${config.cacheFile}`;
+		execShell(codesnipcmd + ' list').then(value => value.trimEnd().split(/\r\n|\n/)).then(items => {
+			const bundleDisposable = vscode.commands.registerCommand('codesnip-vscode.bundle', () => {
+				vscode.window.showQuickPick(items, { placeHolder: "name" }).then(bundle => {
+					if (bundle === undefined) { return; }
 					let cmd = codesnipcmd + ` bundle ${bundle}`;
 					const editor = vscode.window.activeTextEditor;
-					const text = editor?.document.getText();
-					if (text) {
+					const document = editor?.document;
+					const text = document?.getText();
+					if (text !== undefined) {
 						const regex = /^\/\/ codesnip-guard: (\w+)$/gm;
 						let arr;
 						while ((arr = regex.exec(text)) !== null) { cmd += ` -e=${arr[1]}`; }
 					}
 					execShell(cmd).then(value => {
-						editor?.edit((builder) => {
-							const pos: string | undefined = vscode.workspace.getConfiguration().get('codesnip.insertionPosition');
-							if (pos === "cursor") {
-								builder.replace(editor.selection, value.trimEnd() + '\n');
+						editor?.edit(builder => {
+							if (config.insertionPosition === InsertionPosition.cursor) {
+								builder.replace(editor?.selection, value.trimEnd() + '\n');
 							} else {
-								const end = editor.document.lineAt(editor.document.lineCount - 1).range.end;
-								builder.insert(end, value.trimEnd() + '\n');
+								const end = document?.lineAt(document?.lineCount - 1).range.end;
+								if (end !== undefined) { builder.insert(end, value.trimEnd() + '\n'); }
 							}
 						});
 					}).catch(err => {
 						vscode.window.showErrorMessage("Codesnip: " + err);
 					});
-				}
+				});
 			});
+			context.subscriptions.push(bundleDisposable);
+		}).catch(err => { vscode.window.showErrorMessage("Codesnip: " + err); });
+	} catch (err) {
+		const updateItem = "Update Cache";
+		vscode.window.showInformationMessage("Codesnip: cache not found", updateItem).then(selection => {
+			if (selection === updateItem) {
+				vscode.commands.executeCommand("codesnip-vscode.updateCache");
+			}
 		});
-		context.subscriptions.push(bundleDisposable);
-	}).catch(err => { vscode.window.showErrorMessage("Codesnip: " + err); });
-
-	const updateCacheDisposable = vscode.commands.registerCommand('codesnip-vscode.updateCache', () => {
-		let cmd = `cargo codesnip`;
-		const targets: string[] | undefined = vscode.workspace.getConfiguration().get('codesnip.source');
-		targets?.forEach((value) => { cmd += ` -t=${value}`; });
-		const cfgs: string[] | undefined = vscode.workspace.getConfiguration().get('codesnip.cfg');
-		cfgs?.forEach((value) => { cmd += ` --cfg=${value}`; });
-		const fis: string[] | undefined = vscode.workspace.getConfiguration().get('codesnip.filterItem');
-		fis?.forEach((value) => { cmd += ` --filter-item=${value}`; });
-		const fas: string[] | undefined = vscode.workspace.getConfiguration().get('codesnip.filterAttr');
-		fas?.forEach((value) => { cmd += ` --filter-attr=${value}`; });
-		cmd += ` cache ${cache}`;
-		vscode.window.withProgress({
-			location: vscode.ProgressLocation.Notification,
-			title: "Codesnip update cache"
-		}, (progress) => execShell(cmd).then(() => {
-			progress.report({ increment: 1 });
-			vscode.window.showInformationMessage("Codesnip: reload required", "Reload Now").then(() => {
-				vscode.commands.executeCommand("workbench.action.reloadWindow");
-			});
-		}).catch(err => { vscode.window.showErrorMessage("Codesnip: " + err); }));
-	});
-	context.subscriptions.push(updateCacheDisposable);
+	}
 }
 
 export function deactivate() { }
 
-function getCacheFile() {
-	const cache: string | null | undefined = vscode.workspace.getConfiguration().get('codesnip.cacheFile');
-	if (cache !== null && cache !== undefined) {
-		return cache;
-	} else {
-		const folders = vscode.workspace.workspaceFolders;
-		if (folders !== undefined && folders.length >= 1) {
-			return vscode.Uri.joinPath(folders[0].uri, 'target/codesnip/codesnip-cache.json').fsPath;
+interface CodesnipConfiguration {
+	cacheFile: string,
+	source: string[],
+	cfg: string[],
+	filterItem: string[],
+	filterAttr: string[],
+	insertionPosition: InsertionPosition,
+}
+
+enum InsertionPosition {
+	last,
+	cursor
+}
+
+namespace InsertionPosition {
+	export function fromString(str: string | undefined): InsertionPosition {
+		switch (str) {
+			case "cursor": return InsertionPosition.cursor;
+			default: return InsertionPosition.last;
 		}
 	}
 }
 
-function execShell(cmd: string) {
+function getCodesnipConfiguration(): CodesnipConfiguration | undefined {
+	const codesnip = vscode.workspace.getConfiguration('codesnip');
+	let cacheFile = codesnip.get<string | null>('cacheFile', null);
+	if (cacheFile === null) { cacheFile = getDefaultCacheFile(); }
+	if (cacheFile === null) { return undefined; }
+	let source = codesnip.get<string[]>('source', []);
+	let cfg = codesnip.get<string[]>('cfg', []);
+	let filterItem = codesnip.get<string[]>('filterItem', []);
+	let filterAttr = codesnip.get<string[]>('filterAttr', []);
+	let insertionPosition = InsertionPosition.fromString(codesnip.get<string>('insertionPosition'));
+	return { cacheFile, source, cfg, filterItem, filterAttr, insertionPosition };
+}
+
+function getDefaultCacheFile(): string | null {
+	const folders = vscode.workspace.workspaceFolders;
+	if (folders !== undefined && folders.length >= 1) {
+		return vscode.Uri.joinPath(folders[0].uri, 'target/codesnip/codesnip-cache.json').fsPath;
+	} else {
+		return null;
+	}
+}
+
+function getUpdateCacheCommand(config: CodesnipConfiguration): string {
+	let cmd = `cargo codesnip`;
+	config.source.forEach(value => { cmd += ` -t=${value}`; });
+	config.cfg.forEach(value => { cmd += ` --cfg=${value}`; });
+	config.filterItem.forEach(value => { cmd += ` --filter-item=${value}`; });
+	config.filterAttr.forEach(value => { cmd += ` --filter-attr=${value}`; });
+	cmd += ` cache ${config.cacheFile}`;
+	return cmd;
+}
+
+function execShell(cmd: string): Promise<string> {
 	return new Promise<string>((resolve, reject) => {
 		cp.exec(cmd, (err, out) => {
-			if (err) {
-				return reject(err);
-			}
+			if (err) { return reject(err); }
 			return resolve(out);
 		});
 	});
